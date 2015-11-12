ede90f5a4b95e63cfdc21a03c75960a124312be8
Muhammad Saleem <saleem.muhammd@gmail.com>
2015-11-12 03:47:11 +0100

    Update ExecuteTBSSQuery.java

--- Quetsal/src/org/aksw/simba/quetsal/startup/ExecuteTBSSQuery.java
+++ Quetsal/src/org/aksw/simba/quetsal/startup/ExecuteTBSSQuery.java
@@ -1,7 +1,6 @@
 package org.aksw.simba.quetsal.startup;
 
 import  org.openrdf.queryrender.sparql.SPARQLQueryRenderer;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -11,7 +10,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-
 import org.aksw.simba.quetsal.configuration.QuetzalConfig;
 import org.aksw.simba.quetsal.core.Cardinality;
 import org.aksw.simba.quetsal.core.QueryRewriting;
@@ -30,7 +28,6 @@ import org.openrdf.query.parser.sparql.SPARQLParser;
 import org.openrdf.query.parser.sparql.SPARQLParserFactory;
 import org.openrdf.repository.RepositoryException;
 import org.openrdf.repository.sparql.SPARQLRepository;
-
 import com.fluidops.fedx.FedX;
 import com.fluidops.fedx.FederationManager;
 import com.fluidops.fedx.algebra.StatementSource;
@@ -43,56 +40,40 @@ import com.fluidops.fedx.structures.Endpoint;
  */
 public class ExecuteTBSSQuery {
 	public static void main(String[] args) throws Exception {
-		//queryRendererTest();		
 		long strtTime = System.currentTimeMillis();
 		String FedSummaries = "summaries/quetsal-Fedbench-b4.n3";
-		//  String FedSummaries = "C://slices/Linked-SQ-DBpedia-Aidan.ttl";
 		String mode = "ASK_dominant";  //{ASK_dominant, Index_dominant}
 		double commonPredThreshold = 0.33 ;  //considered a predicate as common predicate if it is presenet in 33% available data sources
+		int k = 1; //top-k sources. k=1 means only one  source be selected per triple pattern
 		QuetzalConfig.initialize(FedSummaries,mode,commonPredThreshold);  // must call this function only one time at the start to load configuration information. Please specify the FedSum mode. 
 		System.out.println("One time configuration loading time : "+ (System.currentTimeMillis()-strtTime));
 		FedX fed = FederationManager.getInstance().getFederation();
-		List<Endpoint> members = fed.getMembers();
+		List<Endpoint> members = fed.getMembers();  //list of SPARQL endpoints loaded from index
 		Cache cache =FederationManager.getInstance().getCache();
 		List<String> queries = Queries.getFedBenchQueries();
-		//List<String> queries =  getQueriesFromDir("queries/");
 		SPARQLRepository repo = new SPARQLRepository(members.get(0).getEndpoint());
 		repo.initialize();
-		int tpsrces = 0; 
-		int count = 0;
-		int k = 1; //top-k sources. k=1 means only one  source be selected per triple pattern
-		for (String query : queries)
-		{
+		int tpsrces = 0, count = 0; 
+		for (String query : queries) {
 			System.out.println("-------------------------------------\n"+query);
 			long startTime = System.currentTimeMillis();
 			TBSSSourceSelection sourceSelection = new TBSSSourceSelection(members,cache, query);
 			SPARQLParser parser = new SPARQLParser();
 			ParsedQuery parsedQuery = parser.parseQuery(query, null);
-			//SPARQLQueryRenderer renderer = new SPARQLQueryRenderer();  
-			//query =  renderer.render(parsedQuery);
-			
 			HashMap<Integer, List<StatementPattern>> bgpGroups =  BGPGroupGenerator.generateBgpGroups(parsedQuery);
 			Map<StatementPattern, List<StatementSource>> stmtToSources = sourceSelection.performSourceSelection(bgpGroups);
 			Map<StatementPattern, List<StatementSource>> rankedStmtToSources =  new ConcurrentHashMap<StatementPattern, List<StatementSource>>();
-			//  System.out.println(DNFgrps)
 			System.out.println("Source selection exe time (ms): "+ (System.currentTimeMillis()-startTime));
-          // long rnktime = System.currentTimeMillis();
-			for (StatementPattern stmt : stmtToSources.keySet()) 
+        	for (StatementPattern stmt : stmtToSources.keySet()) 
 			{
-				//tpsrces = tpsrces+ stmtToSources.get(stmt).size();
-				//System.out.println("-----------\n"+stmt);
-				//System.out.println("\n--Random-\n"+stmtToSources.get(stmt));
-				//System.out.println("Cardinaltiy: " +Cardinality.getTriplePatternCardinality(stmt, stmtToSources.get(stmt)));
 			List<StatementSource>	rankedSrces = SourceRanking.getRankedTriplePatternSources(stmt, stmtToSources.get(stmt), k);
 			rankedStmtToSources.put(stmt, rankedSrces) ;
-			//System.out.println("\n--Ranked-\n"+rankedSrces);
 			tpsrces = tpsrces+ rankedStmtToSources.get(stmt).size();
 			}
-			//System.out.println("Source Ranking time (msec):"+ (System.currentTimeMillis()-rnktime));
 			count =  executeQuery(query,bgpGroups,rankedStmtToSources,repo);    //You can uncomment this if you want to execute the query as well. 
-		//	System.out.println(": Query execution time (msec):"+ (System.currentTimeMillis()-startTime));
-		//	System.out.println("Total results: " + count);
-			System.out.println(count + "\t" + (System.currentTimeMillis()-startTime));
+			System.out.println(": Query execution time (msec):"+ (System.currentTimeMillis()-startTime));
+			System.out.println("Total results: " + count);
+		//	System.out.println(count + "\t" + (System.currentTimeMillis()-startTime));
 			Thread.sleep(1000);
 
 		}	
